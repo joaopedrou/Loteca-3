f7aa110e79684a879bcf55fdea8bc9e8379ed604
joaopedrou <andreiacristinacontiribeiro05@gmail.com>
2022-08-31 11:27:48 -0300

    salvar

--- funcoes.js
+++ funcoes.js
@@ -1,41 +1,41 @@
-var numEsc = []
-var numSort =[]
+var numEsco = []
+var numSort = []
+
 function sortNumeros(){
-    for(let i = 0; i < 6; i++){
-   let num = Math.ceil(Math.random() * 60)
+
+    for(let i = 0; i< 6; i++){
+    let num = Math.ceil (Math.random() * 60)
     console.log(num)
-    if(!numSort.includes(num)){
-    numSort[i]=num;
+    if(!numSort.includes(num) ){
+        numSort[i] = num
     }else{
-        i=i-1;
+    i = i -1;
     }
-    }
-    console.log(numSort)
-    document.getElementById("sorteados").indexHTML= numSort;
+}
+document.getElementById("sorteados").innerHTML = numSort;
 contarAcertos();
 }
 
-function lerNumero(value,pos){
-if(value < 1){
-    alert("Numero não pode ser 0 ou negativo seu burro! Digita essa coisa certa!!!!")
-} else if(value > 60){
-    alert("Numeros não podem ser maiores que 60, tu é burro ou oque???")
-}else if(numEsc.includes(value)){
-    alert("Cara... tu é muito burro, o numero tá repetido! Arruma isso ae!!!")
-}else{
-    numEsc[pos]=value
-console.log(numEsc)
-
+function lerNumero(value, pos){
+    if(value < 1){
+        alert("Numero não pode ser zero ou negativo! Digite novamente!")
+    }else if(value > 60){
+        alert("Numero não pode ser maior que 60! Digite novamente!")
+    }else if (numEsco.includes(value)){
+        alert ("Numero repetido! Digite novamente!")
+    }else{
+    numEsco[pos] = value
+    console.log(numEsco)
+    }
+    
 }
 
 function contarAcertos(){
-    let cont = 0;
-    numEsco.forEach(function(value, index){ 
-        if (numSort.includes(value)){
-            cont= cont+1;
+    let cont=0;
+    numEsco.forEach(function(value,index){
+        if(numSort.includes(value)){
+            cont=cont++;
         }
     })
-}
-document.getElementById("acertos").indexHTML = cont;
-
+    document.getElementById("acertos").innerHTML = cont;
 }
